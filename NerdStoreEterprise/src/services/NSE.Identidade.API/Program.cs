e850a9352b9e090887e954cf55baa7599dd29cd6
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-09-16 22:46:30 -0300

    Implementado o Identity, configurações básicas

--- NerdStoreEterprise/src/services/NSE.Identidade.API/Program.cs
+++ NerdStoreEterprise/src/services/NSE.Identidade.API/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NSE.Identidade.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => 
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +30,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
