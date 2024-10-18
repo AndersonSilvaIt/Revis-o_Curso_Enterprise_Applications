8d7d4d4b254f53ff90e5cf39dd121e2d087ba124
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-10-17 22:52:09 -0700

    Início da configuração de autenticação no MVC

---
+++ NerdStoreEterprise/src/web/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Runtime.CompilerServices;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            // Definindo a autenticação do Cliente ( Autenticação por Cookie no cliente );
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/acesso-negado";
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
