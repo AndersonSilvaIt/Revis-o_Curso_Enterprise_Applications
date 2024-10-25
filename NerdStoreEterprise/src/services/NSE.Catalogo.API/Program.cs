26625c10e6288eb1687444822d28306621a18606
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-10-25 14:45:20 -0300

    Configuração de acesso a dados de Catalogo

--- NerdStoreEterprise/src/services/NSE.Catalogo.API/Program.cs
+++ NerdStoreEterprise/src/services/NSE.Catalogo.API/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Data.Repository;
+using NSE.Catalogo.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<CatalogoContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<CatalogoContext>();
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
