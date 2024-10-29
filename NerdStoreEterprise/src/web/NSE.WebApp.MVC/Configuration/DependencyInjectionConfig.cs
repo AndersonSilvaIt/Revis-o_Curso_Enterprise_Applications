3451d07c814530c8c2a14688486c881283bd1105
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-10-28 22:34:17 -0300

    Implementado o Retry para ter resiliência nas APIs

--- NerdStoreEterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -2,6 +2,9 @@
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
+using System;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -13,16 +16,34 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            //services.AddHttpClient<ICatalogoService, CatalogoService>()
-            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+            var retryWaitPoolice = HttpPolicyExtensions
+                                   .HandleTransientHttpError()
+                                   .WaitAndRetryAsync(new[]
+                                   {
+                                       TimeSpan.FromSeconds(1), // Após o primeiro erro, esperar 1 segundo
+                                       TimeSpan.FromSeconds(5), // Após. esperar 5 segundos ...
+                                       TimeSpan.FromSeconds(10)
+                                   },
+                                    (outcome, timespan, retrycount, context) =>
+                                   {
+                                       Console.ForegroundColor = ConsoleColor.Blue;
+                                       Console.WriteLine($"Tentando pela { retrycount} vez! ");
+                                       Console.ForegroundColor = ConsoleColor.White;
+                                   });
 
-            //ICatalogoServiceRefit
-            services.AddHttpClient("Refit", options =>
-            {
-                options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
-            })
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-                .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+                //.AddTransientHttpErrorPolicy(
+                //p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600))); //O Polly irá tentar executar por 3 vezes, até estourar uma exceção
+                .AddPolicyHandler(retryWaitPoolice);
+            //services.AddHttpClient("Refit", options =>
+            //{
+            //    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+            //})
+            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //    .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            // Caso opte por usar o Refir para consumo Rest, basta descomentar o código acima.
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();
