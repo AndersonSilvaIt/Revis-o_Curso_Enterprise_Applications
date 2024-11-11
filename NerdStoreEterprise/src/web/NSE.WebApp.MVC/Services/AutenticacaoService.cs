a84f0d71d9884f79444342200b8bbfc8992ff60a
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-11 20:56:40 -0300

    Conectando o BFF na WebApp

--- NerdStoreEterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ NerdStoreEterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -5,6 +5,12 @@ using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Services
 {
+    public interface IAutenticacaoService
+    {
+        Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+        Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+    }
+
     public class AutenticacaoService : Service, IAutenticacaoService
     {
         private readonly HttpClient _httpClient;
