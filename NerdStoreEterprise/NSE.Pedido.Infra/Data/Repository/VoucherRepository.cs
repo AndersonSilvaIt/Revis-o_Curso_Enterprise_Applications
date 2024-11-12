f26398180fe75fea5afcab4dfe19ffee4865c2fa
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-11 21:31:47 -0300

    Definindo a arquitetura da API de Pedidos

---
+++ NerdStoreEterprise/NSE.Pedido.Infra/Data/Repository/VoucherRepository.cs
@@ -0,0 +1,22 @@
+﻿using NSE.Core.Data;
+using NSE.Pedido.Domain.Vouchers;
+
+namespace NSE.Pedido.Infra.Data.Repository
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        private readonly PedidosContext _context;
+
+        public VoucherRepository(PedidosContext pedidosContext)
+        {
+            _context = pedidosContext;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
