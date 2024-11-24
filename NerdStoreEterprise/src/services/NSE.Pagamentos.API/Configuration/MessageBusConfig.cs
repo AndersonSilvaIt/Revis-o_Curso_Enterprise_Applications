24092fc2493286a8c46391182c6896bc9bb04b6b
AndersonSilvaIt <42393231+AndersonSilvaIt@users.noreply.github.com>
2024-11-24 16:11:45 -0300

    Background service de pagamento

--- NerdStoreEterprise/src/services/NSE.Pagamentos.API/Configuration/MessageBusConfig.cs
+++ NerdStoreEterprise/src/services/NSE.Pagamentos.API/Configuration/MessageBusConfig.cs
@@ -1,12 +1,16 @@
-﻿namespace NSE.Pagamentos.API.Configuration
+﻿using NSE.Core.Utils;
+using NSE.MessageBus;
+using NSE.Pagamentos.API.Services;
+
+namespace NSE.Pagamentos.API.Configuration
 {
     public static class MessageBusConfig
     {
         public static void AddMessageBusConfiguration(this IServiceCollection services,
             IConfiguration configuration)
         {
-            //services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
-            //    .AddHostedService<PagamentoIntegrationHandler>();
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
+                .AddHostedService<PagamentoIntegrationHandler>();
         }
     }
 }
